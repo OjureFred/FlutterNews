569030d794e707af31814db46b41c920d2f8db78
Fredrick Ojure <fredojure@gmail.com>
2022-10-06 12:12:38 +0300

    2. Created first unit test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,34 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutternews/article.dart';
 
 import 'package:flutternews/main.dart';
+import 'package:flutternews/news_change_notifier.dart';
+import 'package:flutternews/news_service.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockNewsService extends Mock implements NewsService {
+  @override
+  Future<List<Article>> getArticles() {
+    // TODO: implement getArticles
+    throw UnimplementedError();
+  }
+ }
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  late NewsChangeNotifier sut;
+  late MockNewsService mockNewsService;
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  setUp(() {
+    mockNewsService = MockNewsService();
+    sut = NewsChangeNotifier(NewsService());
   });
+
+  test(
+    'Initial values are correct',
+      () {
+      expect(sut.articles, []);
+      expect(sut.isLoading, false);
+  }
+  );
 }
